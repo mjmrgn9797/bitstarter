40f19dc8a66205ae24e70d95bae7e8b3a3543a87
mjmrgn9797 <mjmrgn9797@gmail.com>
2013-07-10 05:23:41 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,11 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer(96)
-buffer = fs.readFileSync("~/bitstarter/index.html")
+var buffer = new Buffer(96);
+buffer = fs.readFileSync("~/bitstarter/index.html");
+var bufStr = new String;
+bufStr = buffer.toString('utf-8');
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf-8'));
+  response.send(bufStr);
 });
 
 var port = process.env.PORT || 5000;
