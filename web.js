3a6c800c40b9846f634cc8cd5626982fe1815ac0
mjmrgn9797 <mjmrgn9797@gmail.com>
2013-07-10 05:28:08 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var buffer = new Buffer(96);
-buffer = fs.readFileSync("~/bitstarter/index.html");
+buffer = fs.readFileSync('/bitstarter/index.html');
 var bufStr = new String;
 bufStr = buffer.toString('utf-8');
 
