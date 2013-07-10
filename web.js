cd4a09179aa165ebba899727e4bb031fd030fcfd
mjmrgn9797 <mjmrgn9797@gmail.com>
2013-07-10 20:12:18 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var fs = require('fs');
 // var buffer = fs.readFileSync('/index.html','utf-8');
 // var buffer = 'Hello World';
 app.get('/', function(request, response) {
