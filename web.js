26032782fa186c9b8ce66bad686b38effeb70243
mjmrgn9797 <mjmrgn9797@gmail.com>
2013-07-10 05:49:33 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,13 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer(255);
-buffer = fs.readFileSync('/bitstarter/index.html');
-var bufStr = new String;
-bufStr = buffer.toString('utf-8');
+var buffer = fs.readFileSync('/bitstarter/index.html','utf-8');
 
 app.get('/', function(request, response) {
-  response.send(bufStr);
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
